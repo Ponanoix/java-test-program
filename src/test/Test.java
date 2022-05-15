3850f55691bd2babdc6d8784a3cd6be2165204aa
Ponanoix <waszkiewicz575@gmail.com>
2022-05-15 02:12:17 +0200

    Add a test comment

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,6 +6,6 @@ import java.util.logging.Logger;
 public class Test {
     static Logger writer = Logger.getLogger(Test.class.getName());
     public static void main(String[] args) {
-        writer.log(Level.SEVERE, "Hello, world!");
+        writer.log(Level.SEVERE, "Hello, world!"); //This line prints an information
     }
 }
